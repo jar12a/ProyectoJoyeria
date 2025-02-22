fa4b8f81dc69e2158612bc0228f34ec8f50d1342
Emavilab <ea3192571@gmail.com>
2025-02-22 03:28:49 -0600

    aparezcan las categorias del head

--- continuar_pedido.php
+++ continuar_pedido.php
@@ -155,6 +155,9 @@ include 'bodega/conexionproductos.php';
         }
     </script>
     <script src="https://kit.fontawesome.com/45b2b3afef.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 </body>
 <?php include 'complementos/footer.php';?>
 </html>
