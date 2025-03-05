73f0326e413b417f0522af3ccfa815803cbb1b40
Alex100797 <alex.nunez@uph.edu.hn>
2025-03-04 21:04:33 -0600

    Eliminar llamada innecesaria a session_destroy en logout.php

--- dashboard/logout.php
+++ dashboard/logout.php
@@ -1,6 +1,5 @@
 <?php 
-session_start(); // Iniciar sesión
-session_destroy(); // Destruir la sesión
+session_start(); // Destruir la sesión
 header("Location: login.php"); // Redirigir a la página de inicio
 
 ?>
