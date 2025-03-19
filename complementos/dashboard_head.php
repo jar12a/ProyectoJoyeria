c965ad6e347e7ee7e14074bc5874da98d9771e30
Alex100797 <alex.nunez@uph.edu.hn>
2025-03-18 23:06:40 -0600

    Agregar estilos responsivos y modal de cierre de sesión en el dashboard asi como la creacion del dashboard head y dashboard menu, todos para hacer recursividad de codigo

---
+++ complementos/dashboard_head.php
@@ -0,0 +1,70 @@
+<style>
+        /* Hacer que la tabla sea responsiva */
+        .table-wrapper {
+            overflow-x: auto;
+            -webkit-overflow-scrolling: touch;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        table th,
+        table td {
+            padding: 10px;
+            text-align: left;
+            border: 1px solid #ddd;
+        }
+
+        table th {
+            background-color: #f4f4f4;
+        }
+
+        @media (max-width: 768px) {
+            table {
+                font-size: 12px;
+                /* Reducir el tamaño de la fuente en pantallas pequeñas */
+            }
+
+            table th,
+            table td {
+                padding: 5px;
+            }
+        }
+    </style>
+
+    <!-- Modal de aviso de cierre de sesión -->
+    <div id="modal">
+        <div id="modal-content">
+            <p>Se cerrará la sesión en <span id="countdown">10</span> segundos por inactividad.</p>
+            <button onclick="continuarSesion()">Continuar Sesión</button>
+        </div>
+    </div>
+
+    </div>
+    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+        <!-- Navbar Brand-->
+        <a class="navbar-brand ps-3" href="../index.php">Imperial Gems</a>
+        <!-- Sidebar Toggle para mostrar el menú principal-->
+        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+
+        <!-- Navbar de usuario-->
+        <ul class="navbar-nav ms-auto me-0 me-md-3 my-2 my-md-0">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <?php
+                    echo $nombre; //imprime el nombre de quien esta conectado
+                    ?>
+                    <i class="fas fa-user fa-fw"></i></a>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="#!">Configuración</a></li>
+                    <!--li><a class="dropdown-item" href="#!">Activity Log</a></li-->
+                    <li>
+                        <hr class="dropdown-divider" />
+                    </li>
+                    <li><a class="dropdown-item" href="logout.php">Salir</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
--- dashboard/principal.php
+++ dashboard/principal.php
@@ -22,69 +22,29 @@ $idRol = $_SESSION['idRol'];
     <meta name="author" content="" />
     <title>Imperial Gems</title>
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
-    <link href="css/styles.css" rel="stylesheet" />
+    <link href="../dashboard/css/styles.css" rel="stylesheet" />
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
 
 </head>
 
 <body class="sb-nav-fixed">
+<?php
+        include "../complementos/dashboard_head.php";
+        ?>
+    
+    <!--Barra de navegación -->
+    <div id="layoutSidenav">
+        <?php
+        include "../complementos/dashboard_menu.php";
+        ?>
 
-    <style>
-        /* Hacer que la tabla sea responsiva */
-        .table-wrapper {
-            overflow-x: auto;
-            -webkit-overflow-scrolling: touch;
-        }
-
-        table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-
-        table th,
-        table td {
-            padding: 10px;
-            text-align: left;
-            border: 1px solid #ddd;
-        }
-
-        table th {
-            background-color: #f4f4f4;
-        }
-
-        @media (max-width: 768px) {
-            table {
-                font-size: 12px;
-                /* Reducir el tamaño de la fuente en pantallas pequeñas */
-            }
-
-            table th,
-            table td {
-                padding: 5px;
-            }
-        }
-    </style>
-
-    <!-- Modal de aviso de cierre de sesión -->
-    <div id="modal">
-        <div id="modal-content">
-            <p>Se cerrará la sesión en <span id="countdown">10</span> segundos por inactividad.</p>
-            <button onclick="continuarSesion()">Continuar Sesión</button>
-        </div>
+        <?php
+        include "../complementos/tabla_usuarios.php";
+        ?>
     </div>
-    </div>
-    <?php
-        include "../complementos/dashboard.php";
-    ?>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-    <script src="js/scripts.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-    <script src="assets/demo/chart-area-demo.js"></script>
-    <script src="assets/demo/chart-bar-demo.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
-    <script src="js/datatables-simple-demo.js"></script>
+
 
     <?php
     include "../confi/cierre_sesion.php";
